a27e7de6c9a27f2cfab29bcf5ec87e4b5c909859
Magomimmo <mimmo.cosenza@gmail.com>
2013-09-25 11:39:03 +0200

    url updated

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.clojars.magomimmo/shoreleave-core "0.3.1-SNAPSHOT"
   :description "A smarter client-side with ClojureScript : Shoreleave's core auxiliary functions"
-  :url "http://github.com/shoreleave"
+  :url "https://github.com/magomimmo/shoreleave-core/tree/clojars"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
