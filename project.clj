32c9e518d84f76e8bea2c5fe6f6d4bfa8bac5425
Avicenna <maverickbozo@me.com>
2015-10-16 22:59:46 +0700

    0.3.1-SNAPSHOT and change project name and url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject org.clojars.magomimmo/shoreleave-core "0.3.1"
+(defproject me.maverickbozo/shoreleave-core "0.3.1-SNAPSHOT"
   :description "A smarter client-side with ClojureScript : Shoreleave's core auxiliary functions"
-  :url "https://github.com/magomimmo/shoreleave-core/tree/clojars"
+  :url "https://github.com/mavbozo/shoreleave-core/tree/mavbozo"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
